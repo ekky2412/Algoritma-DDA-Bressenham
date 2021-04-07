d5be1ad25902a6e3adc6bfd499c9dcb10b8589a7
ekky2412 <ekky.raharjo@gmail.com>
2021-04-07 21:30:55 +0700

    tambah datatable

--- bressenhamLingkaran.php
+++ bressenhamLingkaran.php
@@ -1,35 +1,76 @@
 <?php
 
-    function circlePlotPoints($xCenter,$yCenter,$x,$y,&$k,&$nilai){
-        $nilai[$k] = array("x" => $xCenter+$x,"y" => $yCenter+$y); $k++;
-        $nilai[$k] = array("x" => $xCenter-$x,"y" => $yCenter+$y); $k++;
-        $nilai[$k] = array("x" => $xCenter+$x,"y" => $yCenter-$y); $k++;
-        $nilai[$k] = array("x" => $xCenter-$x,"y" => $yCenter-$y); $k++;
-        $nilai[$k] = array("x" => $xCenter+$y,"y" => $yCenter+$x); $k++;
-        $nilai[$k] = array("x" => $xCenter-$y,"y" => $yCenter+$x); $k++;
-        $nilai[$k] = array("x" => $xCenter+$y,"y" => $yCenter-$x); $k++;
-        $nilai[$k] = array("x" => $xCenter-$y,"y" => $yCenter-$x); $k++;
-        // echo "<pre>".var_dump($nilai)."<pre>";
-    }
-    $nilai[0] = 0;
-    $k = 0;
-    $xCenter = $_GET['x1'];
-    $yCenter = $_GET['y1'];
-    $x = 0;
-    $y = $_GET['r'];
-    $p = 1 - $_GET['r'];
-    while($x<$y){
-        $x++;
-        if($p<0){
-            $p = $p + 2*$x + 1;
-        }
-        else{
-            $y--;
-            $p = $p + 2*($x-$y) + 1;
-        }
-        circlePlotPoints($xCenter,$yCenter,$x,$y,$k,$nilai);
-    }
-
-    
+function circlePlotPoints($xCenter, $yCenter, $x, $y, &$k, &$nilai)
+{
+    $nilai[$k] = array("x" => $xCenter + $x, "y" => $yCenter + $y);
+    $k++;
+    $nilai[$k] = array("x" => $xCenter - $x, "y" => $yCenter + $y);
+    $k++;
+    $nilai[$k] = array("x" => $xCenter + $x, "y" => $yCenter - $y);
+    $k++;
+    $nilai[$k] = array("x" => $xCenter - $x, "y" => $yCenter - $y);
+    $k++;
+    $nilai[$k] = array("x" => $xCenter + $y, "y" => $yCenter + $x);
+    $k++;
+    $nilai[$k] = array("x" => $xCenter - $y, "y" => $yCenter + $x);
+    $k++;
+    $nilai[$k] = array("x" => $xCenter + $y, "y" => $yCenter - $x);
+    $k++;
+    $nilai[$k] = array("x" => $xCenter - $y, "y" => $yCenter - $x);
+    $k++;
+    // echo "<pre>".var_dump($nilai)."<pre>";
+}
+$nilai[0] = 0;
+$iterasi = 0;
+$k = 0;
+$xCenter = $_GET['x1'];
+$yCenter = $_GET['y1'];
+$x = 0;
+$y = $_GET['r'];
+$p = 1 - $_GET['r'];
 
 ?>
+<table class="table table-bordered" id="table_id">
+    <thead>
+        <th>k</th>
+        <th>pk</th>
+        <th>xk+1</th>
+        <th>yk+1</th>
+        <th>2xk+1</th>
+        <th>2yk+1</th>
+    </thead>
+    <tr>
+        <td></td>
+        <td></td>
+        <td><?= $x ?></td>
+        <td><?= $y ?></td>
+    </tr>
+        <?php
+
+        while ($x < $y) {
+            ?>
+            <tr>
+            <?php
+            $x++;
+            if ($p < 0) {
+                $p = $p + 2 * $x + 1;
+            } else {
+                $y--;
+                $p = $p + 2 * ($x - $y) + 1;
+            }
+        ?>
+            <td><?= $iterasi ?></td>
+            <td><?= $p ?></td>
+            <td><?= $x ?></td>
+            <td><?= $y ?></td>
+            <td><?= $x * 2 ?></td>
+            <td><?= $y * 2 ?></td>
+        <?php
+            circlePlotPoints($xCenter, $yCenter, $x, $y, $k, $nilai);
+            ?>
+            </tr>
+            <?php
+            $iterasi++;
+        }
+        ?>
+</table>
--- hasil.php
+++ hasil.php
@@ -6,18 +6,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hasil</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css">
+    <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+
 </head>
 
 <body>
-    <?php
-    if ($_GET['tipe'] == "DDA") {
-        include "dda.php";
-    } else if ($_GET['tipe'] == "Bressenham") {
-        include "bressenham.php";
-    } else if($_GET['tipe'] == "BressenhamLingkaran"){
-        include "bressenhamLingkaran.php";
-    }
-    ?>
+    <div>
+        <?php
+        if ($_GET['tipe'] == "DDA") {
+            include "dda.php";
+        } else if ($_GET['tipe'] == "Bressenham") {
+            include "bressenham.php";
+        } else if ($_GET['tipe'] == "BressenhamLingkaran") {
+            include "bressenhamLingkaran.php";
+        }
+        ?>
+    </div>
     <script>
         window.onload = function() {
 
@@ -51,7 +57,14 @@
 
     <div id="chartContainer" style="height: 370px; width: 100%;"></div>
     <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
+ 
 
+    <script>
+        $(document).ready( function () {
+    $('#table_id').DataTable();
+} );
+    </script>
 </body>
 
 </html>
